325845b8a97bbab8738f4b9d0e0978ea06c54b4a
Vega Feng <vega.feng@oocl.com>
2025-09-08 18:51:38 +0800

    feat(day6): add test for fetch car with used ticket via standardParkingLotBoy

--- src/test/java/com/afs/parkinglot/StandardParkingLotBoyTest.java
+++ src/test/java/com/afs/parkinglot/StandardParkingLotBoyTest.java
@@ -29,5 +29,16 @@ public class StandardParkingLotBoyTest {
         WrongTicketException wrongTicketException = assertThrows(WrongTicketException.class, ()->standardParkingLotBoy.getParkingLots().getFirst().fetch(car, wrongTicket));
         assertEquals("Wrong ticket", wrongTicketException.getMessage());
     }
+    @Test
+    void should_throw_exception_when_fetch_given_used_ticket() throws Exception {
+        StandardParkingLotBoy standardParkingLotBoy = new StandardParkingLotBoy();
+        ParkingLot parkingLot = new ParkingLot();
+        standardParkingLotBoy.setParkingLot(parkingLot);
+        Car car = new Car(1);
+        ParkingTicket parkTicket = standardParkingLotBoy.getParkingLots().getFirst().park(car);
+        standardParkingLotBoy.getParkingLots().getFirst().fetch(car, parkTicket);
+        UsedTicketException usedTicketException = assertThrows(UsedTicketException.class, ()->standardParkingLotBoy.getParkingLots().getFirst().fetch(car, parkTicket));
+        assertEquals("Ticket is already used", usedTicketException.getMessage());
+    }
 
 }
